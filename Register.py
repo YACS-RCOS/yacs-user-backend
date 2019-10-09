1137135934b9ef2d3c43cfef3ae94cdf048de470
maoyuwang <github@maoyu.wang>
2019-10-09 00:35:40 -0400

    Exception handler for Register

--- Register.py
+++ Register.py
@@ -5,12 +5,18 @@ from database import DB
 def register(form):
     db = DB()
 
-    name = form['name']
-    email = form['email']
-    phone = form['phone']
-    password = form['password']
-    major = form['major']
-    degree = form['degree']
+    try:
+        name = form['name']
+        email = form['email']
+        phone = form['phone']
+        password = form['password']
+        major = form['major']
+        degree = form['degree']
+    except KeyError:
+        return msg.errMsg("Please check your request keys.")
+
+    if password.split() == "":
+        return msg.errMsg("Password cannot be empty.")
 
     if len(db.getUser(email=email)) != 0:
         return msg.errMsg("User alreadt exists.")
